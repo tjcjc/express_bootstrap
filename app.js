afffb07c8157e9e3d833c63799b5e9daaedd56e7
tjcjc <taijcjc@gmail.com>
2011-12-25 13:27:14 +0800

    add bootstrap

--- app.js
+++ app.js
@@ -42,6 +42,8 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/fluid', routes.fluid);
+app.get('/container', routes.container);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,9 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+exports.fluid = function(req, res){
+  res.render('fluid', { title: 'Express' })
+};
+exports.container = function(req, res){
+  res.render('container', { title: 'Express' })
+};
